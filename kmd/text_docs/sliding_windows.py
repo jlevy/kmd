ce6b7ae0a9a5b3d55127e87833bd9d8ed077e608
Joshua Levy <joshua@cal.berkeley.edu>
2024-06-13 10:23:36 -0700

    Remove no longer needed truncate at wordtok.

--- kmd/text_docs/sliding_windows.py
+++ kmd/text_docs/sliding_windows.py
@@ -11,47 +11,13 @@ from kmd.model.items_model import Format
 from kmd.text_formatting.doc_formatting import normalize_formatting
 from kmd.text_docs.text_doc import (
     SentIndex,
-    Sentence,
     TextDoc,
     Unit,
     size,
 )
-from kmd.text_docs.wordtoks import (
-    join_wordtoks,
-    raw_text_to_wordtoks,
-)
 
 
 log = get_logger(__name__)
-
-
-def _truncate_sent_at_wordtok_offset(sent: Sentence, offset: int) -> Sentence:
-    """
-    Truncate a sentence to the given number of wordtoks.
-    """
-    wordtoks = raw_text_to_wordtoks(sent.text)
-    truncated_wordtoks = wordtoks[:offset]
-    joined = join_wordtoks(truncated_wordtoks)
-    return Sentence(joined, sent.char_offset)
-
-
-def truncate_at_wordtok_offset(doc: TextDoc, offset: int) -> TextDoc:
-    """
-    Truncate a document at a given wordtok offset.
-    """
-    index, _ = doc.seek_to_sent(offset, Unit.WORDTOKS)
-    try:
-        sub_doc = doc.sub_doc(SentIndex(0, 0), doc.prev_sent(index))
-    except ValueError:
-        # Offset is within the first sentence.
-        sub_doc = TextDoc([])
-    current_size = sub_doc.size(Unit.WORDTOKS)
-    last_sent = doc.paragraphs[index.para_index].sentences[index.sent_index]
-    remaining_wordtoks = offset - current_size - 1
-    if remaining_wordtoks > 0:
-        truncated_sent = _truncate_sent_at_wordtok_offset(last_sent, remaining_wordtoks)
-        sub_doc.append_sent(truncated_sent)
-    return sub_doc
 
 
 def sliding_word_window(
@@ -141,27 +107,3 @@ def test_sliding_window():
         assert size(sub_text, Unit.BYTES) <= window_size
 
         assert sub_text in doc.reassemble()
-
-
-def test_truncate_at_wordtok():
-    # Sentence truncation.
-    sent = Sentence("This is a test sentence.", 999)
-    truncated_sent = _truncate_sent_at_wordtok_offset(sent, 0)
-    assert truncated_sent.text == ""
-
-    truncated_sent = _truncate_sent_at_wordtok_offset(sent, 7)
-    assert truncated_sent.text == "This is a test"
-    assert truncated_sent.char_offset == 999
-
-    # Doc truncation.
-    doc = TextDoc.from_text(_example_text)
-    truncated_doc = truncate_at_wordtok_offset(doc, 10)
-    expected_text = "This is the first paragraph."
-    expected_doc = TextDoc.from_text(expected_text)
-    assert truncated_doc.reassemble() == expected_doc.reassemble()
-
-    truncated_doc = truncate_at_wordtok_offset(doc, 34)
-    expected_text = "This is the first paragraph. It has multiple sentences.\n\nThis is the second paragraph. It also"
-    print(truncated_doc.reassemble())
-    expected_doc = TextDoc.from_text(expected_text)
-    assert truncated_doc.reassemble() == expected_doc.reassemble()
