ef4921d4b8b12178eeacaf0181209edd92fc6802
Joshua Levy <joshua@cal.berkeley.edu>
2024-09-13 11:47:34 -0700

    Fixups to Url lib.

--- kmd/util/url.py
+++ kmd/util/url.py
@@ -1,16 +1,17 @@
+"""
+Simple URL handling with minimal dependencies.
+"""
+
+import re
 from pathlib import Path
 from typing import NewType, Optional
 from urllib.parse import urlparse, urlsplit, urlunsplit
 
-from kmd.config.logger import get_logger
-
-
-log = get_logger(__name__)
 
 Url = NewType("Url", str)
 """
-A minimal URL type that functions like a string but allows for better clarity
-and type checking.
+A minimalist URL type that can be used in place of a string but allows
+for better clarity and type checking.
 """
 
 
@@ -28,34 +29,62 @@ def is_url(text: str, http_only: bool = False) -> bool:
         return False
 
 
-def normalize_url(url: Url, expect_http=True, remove_fragment=True) -> Url:
+def is_file_url(url: str | Url) -> bool:
     """
-    Minimal URL normalization. By default also enforces http/https and removes fragment.
+    Is URL a file:// URL?
     """
-    # urlsplit is too forgiving.
-    if expect_http and not url.startswith("http://") and not url.startswith("https://"):
-        raise ValueError(f"Expected http/https URL but found: {url}")
-
-    scheme, netloc, path, query, fragment = urlsplit(url)
-
-    if remove_fragment:
-        fragment = None
-    normalized_url = urlunsplit((scheme, netloc, path, query, fragment))
-    if url != normalized_url:
-        log.info("Normalized URL: %s -> %s" % (url, normalized_url))
-    return Url(normalized_url)
-
-
-def as_file_url(path: Path) -> Url:
-    return Url(f"file://{path.resolve()}")
+    return url.startswith("file://")
 
 
 def parse_file_url(url: Url) -> Optional[Path]:
+    """
+    Parse a file URL and return the path, or None if not a file URL.
+    """
     parsed_url = urlparse(url)
     if parsed_url.scheme == "file":
         return Path(parsed_url.path)
     else:
         return None
+
+
+def as_file_url(path: Path | str) -> Url:
+    """
+    Resolve a path as a file:// URL. Resolves relative paths to absolute paths on the local filesystem.
+    """
+    if is_file_url(str(path)):
+        return Url(str(path))
+    else:
+        abs_path = Path(path).resolve()
+        return Url(f"file://{abs_path}")
+
+
+def normalize_url(
+    url: Url, http_or_file_only=True, drop_fragment=True, resolve_local_paths=True
+) -> Url:
+    """
+    Minimal URL normalization. By default also enforces http/https/file URLs and removes fragment.
+    """
+    # urlsplit is too forgiving.
+    if (
+        http_or_file_only
+        and not url.startswith("http://")
+        and not url.startswith("https://")
+        and not is_file_url(url)
+    ):
+        raise ValueError(f"Expected http:// or https:// or file:// URL but found: {url}")
+
+    scheme, netloc, path, query, fragment = urlsplit(url)
+
+    if drop_fragment:
+        fragment = None
+    if path == "/":
+        path = ""
+    if scheme == "file" and path and resolve_local_paths:
+        path = str(Path(path).resolve())
+
+    normalized_url = urlunsplit((scheme, netloc, path, query, fragment))
+
+    return Url(normalized_url)
 
 
 ## Tests
@@ -74,3 +103,37 @@ def test_is_url():
     assert is_url("https://example.com", http_only=True) == True
     assert is_url("ftp://example.com", http_only=True) == False
     assert is_url("file:///path/to/file", http_only=True) == False
+
+
+def test_as_file_url():
+    assert as_file_url("file:///path/to/file") == "file:///path/to/file"
+    assert as_file_url("/path/to/file") == "file:///path/to/file"
+    assert re.match(r"file:///.*/path/to/file", as_file_url("path/to/file"))
+
+
+def test_normalize_url():
+    assert normalize_url(Url("http://example.com")) == "http://example.com"
+    assert normalize_url(Url("http://www.example.com/")) == "http://www.example.com"
+    assert normalize_url(Url("https://example.com")) == "https://example.com"
+    assert (
+        normalize_url(Url("https://example.com/foo/bar.html#fragment"), drop_fragment=True)
+        == "https://example.com/foo/bar.html"
+    )
+    assert (
+        normalize_url(Url("https://example.com#fragment"), drop_fragment=False)
+        == "https://example.com#fragment"
+    )
+    assert normalize_url(Url("file:///path/to/file/")) == "file:///path/to/file"
+    assert (
+        normalize_url(Url("file:///path/to/file#fragment"), drop_fragment=True)
+        == "file:///path/to/file"
+    )
+    assert (
+        normalize_url(Url("file:///path/to/file#fragment"), drop_fragment=False)
+        == "file:///path/to/file#fragment"
+    )
+    try:
+        normalize_url(Url("ftp://example.com"))
+        assert False
+    except ValueError as e:
+        assert str(e) == "Expected http:// or https:// or file:// URL but found: ftp://example.com"
