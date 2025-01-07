bf9883ad81a9161bc9d30a56ce8fe9bdf972240a
Joshua Levy <joshua@cal.berkeley.edu>
2025-01-07 11:52:38 -0800

    More search commands.

--- kmd/commands/web_commands.py
+++ kmd/commands/web_commands.py
@@ -15,16 +15,48 @@ def browser(url_or_query: str) -> None:
 @kmd_command
 def search_google(query: str) -> None:
     """
-    Search Google for a query.
+    Open browser with a Google for a query.
     """
     native_open_url(f"https://www.google.com/search?q={urllib.parse.quote(query)}")
 
 
 @kmd_command
+def search_duckduckgo(query: str) -> None:
+    """
+    Open browser with a DuckDuckGo search for a query.
+    """
+    native_open_url(f"https://duckduckgo.com/?q={urllib.parse.quote(query)}")
+
+
+@kmd_command
+def search_bing(query: str) -> None:
+    """
+    Open browser with a Bing search for a query.
+    """
+    native_open_url(f"https://www.bing.com/search?q={urllib.parse.quote(query)}")
+
+
+@kmd_command
 def search_youtube(query: str) -> None:
     """
-    Search YouTube for a query.
+    Open browser with a YouTube search for a query.
     """
     params = {"search_query": query}
     url = f"https://www.youtube.com/results?{urllib.parse.urlencode(params)}"
     native_open_url(url)
+
+
+@kmd_command
+def search_amazon(query: str) -> None:
+    """
+    Open browser with an Amazon search for a query.
+    """
+    native_open_url(f"https://www.amazon.com/s?k={urllib.parse.quote(query)}")
+
+
+@kmd_command
+def search_perplexity(query: str) -> None:
+    """
+    Open browser with a Perplexity search for a query.
+    """
+    native_open_url(f"https://www.perplexity.ai/search?q={urllib.parse.quote(query)}")
